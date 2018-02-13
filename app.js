df3b5566d6512e65c030a8e4ee639e14f342770a
krishnan2098 <krishnan2098@gmail.com>
2018-02-14 00:31:41 +0530

    Added Partials, converted html to ejs files

--- app.js
+++ app.js
@@ -2,20 +2,25 @@ var express = require("express");
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
 app.set('views', __dirname + '/public/views');
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+app.set('view engine', 'ejs');
+
+var user = [{
+    UserName: "Admin",
+    Password: "Password123"
+}]
 
 app.get("/", function(req, res){
-    res.render("index.html");
-    console.log("reached home!");
+    res.render("index.ejs");
+    console.log("Home!");
 });
 
 app.get("/login", function(req, res){
-    res.render("login.html");
+    res.render("login.ejs");
+    console.log("Login!");
 });
 
 app.listen(app.get("port"), function(){
-   console.log("Server has started at PORT:3000. Enter the following address to view: 0.0.0:3000/")
+   console.log("Server has started at PORT:5000. Enter the following address to view: localhost:5000/")
 });
