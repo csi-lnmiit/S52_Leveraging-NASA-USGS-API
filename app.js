97ea160fa50955cfc8815e8323dcf17073f163f2
krishnan2098 <krishnan2098@gmail.com>
2018-02-14 01:04:18 +0530

    Added dummy user details to test authentication

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static('public'));
@@ -7,19 +9,37 @@ app.set('views', __dirname + '/public/views');
 app.set('view engine', 'ejs');
 
 var user = [{
-    UserName: "Admin",
+    Email: "admin@admin.com",
     Password: "Password123"
 }]
 
 app.get("/", function(req, res){
-    res.render("index.ejs");
+    res.render("index");
     console.log("Home!");
 });
 
 app.get("/login", function(req, res){
-    res.render("login.ejs");
+    res.render("login");
     console.log("Login!");
 });
+
+app.post("/authenticateUser", function(req, res){
+//     var userDetails = [{
+//         Email: req.body.email,
+//         Password: req.body.pwd
+//     }];
+//     console.log(userDetails.Password);
+//     console.log(user.Password);
+//     if(userDetails.Password === user.Password){
+//         res.redirect("dashboard");
+//         console.log("Login success");
+//     } else {
+//         res.redirect("login");
+//         console.log("Login failed!");
+//     }
+    console.log("Authentication Occuring!")
+    res.redirect("login");
+ });
 
 app.listen(app.get("port"), function(){
    console.log("Server has started at PORT:5000. Enter the following address to view: localhost:5000/")
