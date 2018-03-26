baf82bf9ee04e191262cba921423c1b42294b5ba
krishnan2098 <krishnan2098@gmail.com>
2018-03-26 20:19:39 +0530

    Refactored directories and added passport configuration

--- app.js
+++ app.js
@@ -8,18 +8,29 @@ var express = require("express"),
 mongoose.connect("mongodb://localhost/CSI_app");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('port', (process.env.PORT || 5000));
-app.use('views', __dirname + '/public');
+app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
-var user = [{
-    Email: "admin@admin.com",
-    Password: "Password123"
-}]
+//Seed data must be called here
+
+//Passport Config
+app.use(require("express-session")({
+    secret: "This is the secret key!",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
     res.render("landing");
     console.log("Home!");
 });
+
 //show login form
 app.get("/login", function(req, res){
     res.render("login");
@@ -30,24 +41,6 @@ app.get("/login", function(req, res){
 app.post("/login", function(req, res){
    res.send("Post Route reached");
 });
-
-app.post("/authenticateUser", function(req, res){
-//     var userDetails = [{
-//         Email: req.body.email,
-//         Password: req.body.pwd
-//     }];
-//     console.log(userDetails.Password);
-//     console.log(user.Password);
-//     if(userDetails.Password === user.Password){
-//         res.redirect("dashboard");
-//         console.log("Login success");
-//     } else {
-//         res.redirect("login");
-//         console.log("Login failed!");
-//     }
-    console.log("Authentication Occuring!")
-    res.redirect("login");
- });
 
 app.listen(app.get("port"), function(){
    console.log("Server has started at PORT:5000. Enter http://127.0.0.1:5000 or http://localhost:5000 to view")
